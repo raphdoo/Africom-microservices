c7eec65f0f1b7d5e6ed4d94a15b4a83aa44c962d
Rapheal Ajiboye <raphoooo95@gmail.com>
2023-10-06 11:30:33 +0100

    setting up expiration service index and docker file

---
+++ expiration/src/__mocks__/nats-wrapper.ts
@@ -0,0 +1,13 @@
+//faking nats wrapper implementation for jest
+
+export const natsWrapper = {
+  client: {
+    publish: jest
+      .fn()
+      .mockImplementation(
+        (subject: string, data: string, callback: () => void) => {
+          callback();
+        }
+      ),
+  },
+};
